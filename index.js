e77f0607c0a18a9fd96e75a1e5039fe5e7fc28a5
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-06 03:32:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-  var newArray = [];
+  var arr = [];
   for(var i = 0, 1 = musicians.length, i < 1; i++){
-    
+    arr.push(`${musicians[i]} plays ${instruments[i]}')
   }
   
 }
